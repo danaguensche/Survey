a8b7ff0c102f52b2ea0b7e55b527d229eedd3366
danaguensche <dana.guensche@ausbildung.diakonie-am-campus.de>
2026-03-04 10:48:52 +0100

    home

--- resources/js/Pages/HomeRoute.vue
+++ resources/js/Pages/HomeRoute.vue
@@ -1,17 +1,18 @@
 <template>
-    <div>
-        <h2>HOME</h2>
-        <router-link to="/test"> Take me to Test page </router-link>
-        <button @click.prevent="tiggerEndpoint">Trigger Endpoint</button>
-        <p v-if="response">{{ response.data }}</p>
-        <p v-else>No response yet.</p>
+    <div class="text-center mb-8">
+        <h1 class="text-3xl font-bold text-gray-800 mb-2">Zufriedenheitsumfrage für Auszubildende der Diakonie
+            am Campus</h1>
+        <p class="text-gray-600">Ihre Meinung ist uns wichtig! Bitte nehmen Sie sich 5 Minuten Zeit, um diese
+            anonyme Umfrage auszufüllen.</p>
+        <p>Bewertung der Maßnahme im aktuellen Ausbildungsjahr</p>
+        <div class="progress-bar mt-6">
+            <div class="progress" id="progressBar" style="width: 0%"></div>
+        </div>
     </div>
 </template>
 
 <script>
 
-//test git connection
-//test git connection 2
 import axios from 'axios';
 import { ref } from 'vue';
 
