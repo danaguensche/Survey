3e4e26f2915cfad556aa40f1b192d2d8337ab783
danaguensche <dana.guensche@ausbildung.diakonie-am-campus.de>
2026-03-05 10:10:06 +0100

    upsi

--- app/Http/Controllers/SurveyController.php
+++ app/Http/Controllers/SurveyController.php
@@ -7,5 +7,26 @@ use Illuminate\Http\Request;
 
 class SurveyController extends Controller
 {
-    //
+    public function store(Request $request)
+    {
+        $request->validate([
+            'answers'               => 'required|array',
+            'answers.*.question_id' => 'required|exists:survey_questions,id',
+            'answers.*.rating_value' => 'nullable|integer|min:1|max:6',
+            'answers.*.text_value'  => 'nullable|string|max:2000',
+        ]);
+
+        $submission = SurveySubmission::create([
+            'ausbildungsberuf' => $request->ausbildungsberuf,
+            'ausbildungsjahr'  => $request->ausbildungsjahr,
+            'datum'            => $request->datum,
+            'consent'          => $request->consent ?? false,
+        ]);
+
+        foreach ($request->answers as $answer) {
+            $submission->answers()->create($answer);
+        }
+
+        return response()->json(['message' => 'Umfrage gespeichert'], 201);
+    }
 }
